5026040ca0653aa201bb044aaa7b0a61db717a81
kimjin <hjklkj12@gmail.com>
2022-10-28 23:10:07 +0900

    lecture 10/28

---
+++ study/sort_performance_compare.py
@@ -0,0 +1,31 @@
+from random import randint
+import time
+
+array = []
+for _ in range(10000) :
+    array.append(randint(1,100))
+
+start_time = time.time()
+
+for i in range(len(array)):
+    min_index = i
+    for j in range(i+1, len(array)) :
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
+
+end_time = time.time()
+
+print(f"선택정렬 성능 = {end_time - start_time}")
+
+array = []
+for _ in range(10000) :
+    array.append(randint(1,100))
+
+start_time = time.time()
+
+array.sort()
+
+end_time = time.time()
+
+print(f"기본 정렬 성능 = {end_time - start_time}")
